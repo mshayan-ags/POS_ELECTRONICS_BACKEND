9cc4dd196048655d1e4921d10cada35784297276
mshayan-ags <mshayan4002@gmail.com>
2022-09-24 15:12:52 +0500

    Quantity Available

--- src/utils/Calculate.js
+++ src/utils/Calculate.js
@@ -35,10 +35,10 @@ async function QuantityTotal(Products, identifier, prisma) {
 
 		let CurrQuantityAvailable = Number(Number(PurchaseQuantity) - Number(ReturnPurchaseQuantity)) - Number(Number(SaleQuantity) - Number(SaleReturnQuantity));
 
-		if (Number(ExtractProduct.QuantityAvailable) !== Number(CurrQuantityAvailable)) {
+		// if (Number(ExtractProduct.QuantityAvailable) !== Number(CurrQuantityAvailable)) {
 			ExtractProduct.QuantityAvailable = CurrQuantityAvailable
 			AllProducts.push(ExtractProduct);
-		}
+		// }
 	}
 
 	for (let index = 0; index < AllProducts.length; index++) {
